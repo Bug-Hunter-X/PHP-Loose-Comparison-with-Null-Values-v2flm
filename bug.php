a47c724de0f88b716c37ab4a47dd2ab7d1b57735
BugHunter-X <askzenai@gmail.com>
2025-01-31 09:52:05 +0100

    Add bug.php file

---
+++ bug.php
@@ -0,0 +1,20 @@
+```php
+function myFunction($param) {
+  if ($param === null) {
+    return 'null';
+  }
+  return $param;
+}
+
+$result = myFunction(null);
+echo $result; // Output: null
+
+$result = myFunction(0);
+echo $result; // Output: 0
+
+$result = myFunction('');
+echo $result; // Output: 
+
+$result = myFunction(false);
+echo $result; // Output: 
+```
